136cbdfe9f4eebfe190272a80c81f60de0160b11
Sergio Orizano Salvador <svorizano@gmail.com>
2024-11-21 22:19:41 -0500

    response

--- API_IC.py
+++ API_IC.py
@@ -7,6 +7,9 @@ SUPABASE_URL = "https://pbsjxezvcmdlwusveukd.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBic2p4ZXp2Y21kbHd1c3ZldWtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY1MzYxMjgsImV4cCI6MjA0MjExMjEyOH0.LPj5kYWsWQ-wfLry8FBTpXiJxUUVI5qNCTL2CBKMxYY"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
+response = supabase.table("exchange_rates").select("*").execute()
+print(response)
+
 # Consumir el API de Yahoo Finance usando yfinance
 def get_exchange_rate(base_currency="USD", target_currency="EUR"):
     pair = f"{base_currency}{target_currency}=X"
